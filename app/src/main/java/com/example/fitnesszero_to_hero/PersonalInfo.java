83c7976463f89e20ffa55fa8f87e40dfd8e44a43
L'Bob <jonasnikolailj@gmail.com>
2022-10-19 12:48:05 +0200

    Made the skeleton for Profile page and personal info page.
    Made a skeleton for profile page, with link to relevant other pages, and made the profile info page, where you can see info about yourself and change it (Later implementation after DB is ready.)

---
+++ app/src/main/java/com/example/fitnesszero_to_hero/PersonalInfo.java
@@ -0,0 +1,72 @@
+package com.example.fitnesszero_to_hero;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class PersonalInfo extends AppCompatActivity {
+
+    boolean editing = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_info);
+
+        EditText textName = (EditText) findViewById(R.id.personalInfoName);
+        EditText textEmail = (EditText)findViewById(R.id.personalInfoMail);
+        EditText textPhone = (EditText)findViewById(R.id.personalInfoPhone);
+
+        textName.setEnabled(false);
+        textEmail.setEnabled(false);
+        textPhone.setEnabled(false);
+    }
+
+    //For changing personal info
+    public void editInfo(View view){
+        EditText textName = (EditText) findViewById(R.id.personalInfoName);
+        EditText textEmail = (EditText)findViewById(R.id.personalInfoMail);
+        EditText textPhone = (EditText)findViewById(R.id.personalInfoPhone);
+
+        Button editBTN = (Button)findViewById(R.id.personalInfoEditBTN);
+        Button abbBTN = (Button)findViewById(R.id.personalInfoAbnBTN);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(PersonalInfo.this);
+        builder.setMessage("Are you sure you want to save?");
+
+        //Dialog Listener for Builder
+        DialogInterface.OnClickListener dialogListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                textName.setEnabled(false);
+                textEmail.setEnabled(false);
+                textPhone.setEnabled(false);
+                abbBTN.setVisibility(View.VISIBLE);
+                editBTN.setText("Ændre Oplysninger");
+                editing = false;
+            }
+        };
+
+        if (editing == false){
+
+            textName.setEnabled(true);
+            textEmail.setEnabled(true);
+            textPhone.setEnabled(true);
+            abbBTN.setVisibility(View.GONE);
+            editBTN.setText("Save");
+            editing = true;
+        }
+        else if (editing){
+
+            builder.setPositiveButton("Save", dialogListener);
+            builder.show();
+
+        }
+    }
+}
--- app/src/main/java/com/example/fitnesszero_to_hero/PersonalMain.java
+++ app/src/main/java/com/example/fitnesszero_to_hero/PersonalMain.java
@@ -2,7 +2,9 @@ package com.example.fitnesszero_to_hero;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class PersonalMain extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class PersonalMain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_main);
+
+
+    }
+
+    public void gotoPersonalInfo(View view){
+        Intent intent = new Intent(PersonalMain.this, PersonalInfo.class);
+        startActivity(intent);
     }
 }
